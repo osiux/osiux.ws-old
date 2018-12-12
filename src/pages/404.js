8aae57d63d5527693ee4f825cc36fd82d5f2a1b5
Eduardo Reveles <me@osiux.ws>
2018-12-11 23:26:21 -0600

    404 page and more content on landing

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 import SEO from '../components/seo';
+import Salem from '../components/images/salem';
 
 const NotFoundPage = () => (
     <>
         <SEO title="404: Not found" />
-        <h1>NOT FOUND</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <main
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                minHeight: '100vh',
+                justifyContent: 'center',
+                padding: '0 30px',
+                textAlign: 'center',
+                background: '#121619',
+                color: '#f6f7eb',
+            }}
+        >
+            <Salem />
+            <h1 style={{ color: '#f6f7eb' }}>NOT FOUND</h1>
+            <p>
+                You just hit a route that doesn&#39;t exist... the sadness.
+                Maybe go to the <Link to="/">main page</Link>?
+            </p>
+        </main>
     </>
 );
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,38 @@
 import React from 'react';
+import ReactHintFactory from 'react-hint';
 
 import SEO from '../components/seo';
+import Dog from '../components/images/dog';
+import Cats from '../components/images/cats';
 
-import '../styles/normalize.css';
+import 'react-hint/css/index.css';
 import '../styles/styles.css';
+
+const ReactHint = ReactHintFactory(React);
+
+const onRenderContent = (target, content) => {
+    const { rhImage } = target.dataset;
+
+    return (
+        <div className="react-hint__image">
+            {rhImage === 'cats' ? <Cats /> : <Dog />}
+            <p>Amelia</p>
+        </div>
+    );
+};
 
 const IndexPage = () => (
     <>
         <SEO
             title="Home"
             keywords={['landing', 'developer', 'react', 'frontend']}
+        />
+        <ReactHint autoPosition events />
+        <ReactHint
+            autoPosition
+            events
+            attribute="data-rh-image"
+            onRenderContent={onRenderContent}
         />
         <main
             style={{
@@ -23,20 +46,38 @@ const IndexPage = () => (
                 color: '#f6f7eb',
             }}
         >
-            <p
+            <h1
                 style={{
                     fontSize: '3.75em',
                     fontWeight: '600',
+                    color: '#f6f7eb',
                 }}
             >
                 Hi! I'm Eduardo.
-            </p>
+            </h1>
             <p>
-                Frontend developer living in Mexico City, currently working
-                remotely at{' '}
-                <a href="https://gumgum.com/sports">GumGum Sports</a>. Sometimes
-                I like to <a href="https://log.osiux.ws/">write</a> (mostly in
-                spanish).
+                <span data-rh="Laravel, React">Full stack developer</span>{' '}
+                living in Mexico City, currently working remotely at{' '}
+                <a href="https://gumgum.com/sports">GumGum Sports</a>.
+                <br />
+                Proudly married to a great{' '}
+                <a href="https://www.instagram.com/melissacastillo_photo/">
+                    photographer
+                </a>
+                , we have{' '}
+                <span data-rh-image="cats" data-rh-image-at="bottom">
+                    3 cats
+                </span>{' '}
+                and{' '}
+                <span data-rh-image="dog" data-rh-image-at="bottom">
+                    a dog
+                </span>
+                .
+                <br />
+                Sometimes I like to <a href="https://log.osiux.ws/">
+                    write
+                </a>{' '}
+                (mostly in spanish).
             </p>
             <ul
                 style={{
